dddd4a47874ae4a2c80f7c8edacb3c5859c0a1e8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-11-23 02:23:30 +0900

    01-hello-world

--- 00-introduction.exs
+++
@@ -1,3 +0,0 @@
-# https://elixir-lang.org/getting-started/introduction.html
-
-IO.puts("Hello world from Elixir")
---
+++ 01-hello-world.exs
@@ -0,0 +1,3 @@
+# https://hexdocs.pm/elixir/1.16/introduction.html
+
+IO.puts("Hello world from Elixir")
