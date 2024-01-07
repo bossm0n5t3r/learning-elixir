f8d6b13ac81157ae4e16e3bbdae934e7498c2a1c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-08 02:39:10 +0900

    25-set.exs

---
+++ 25-set.exs
@@ -0,0 +1,30 @@
+ExUnit.start()
+
+defmodule SetTest do
+  use ExUnit.Case
+
+  test "to_list" do
+    list = Enum.into([1, 2, 2, 3], MapSet.new()) |> MapSet.to_list()
+
+    assert list == [1, 2, 3]
+  end
+
+  test "union" do
+    union =
+      Enum.into([1, 2, 3], MapSet.new()) |> MapSet.union(Enum.into([2, 3, 4], MapSet.new()))
+
+    assert MapSet.to_list(union) == [1, 2, 3, 4]
+  end
+
+  test "intersection" do
+    union =
+      Enum.into([1, 2, 3], MapSet.new())
+      |> MapSet.intersection(Enum.into([2, 3, 4], MapSet.new()))
+
+    assert MapSet.to_list(union) == [2, 3]
+  end
+
+  test "member?" do
+    refute Enum.into([1, 3, 5], MapSet.new()) |> MapSet.member?(2)
+  end
+end
