9c307fa4589a33543c5c1d1a4802097f70f989e3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-01-08 03:43:20 +0900

    28-frank-3.exs

---
+++ 28-frank-3.exs
@@ -0,0 +1,150 @@
+# A micro web DSL library called "Frank"
+# The motivation was mainly to learn about macros.
+#
+# The third (and hopefully final) version of Frank
+# adds support for matching on parameters.
+# (Still no regex match, but I realized I didn't care
+# as much about that as I thought :-)
+
+require Record
+
+defmodule Frank do
+  @moduledoc """
+  Frank is a micro web library that provides a DSL for defining routes.
+
+      defmodule MyApp do
+        use Frank
+
+        get "/foo" do
+          response 200, "foo!"
+        end
+
+        get "/hello/:name" do
+          response 200, "greetings \#{name}"
+        end
+      end
+
+      Frank.sing(MyApp)
+
+  To run:
+
+  $ iex 28-frank-3.exs
+
+  ... then point your browser to http://localhost:3000
+  """
+
+  Record.defrecord(:mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl"))
+
+  @doc """
+    Start the web server given the app module.
+  """
+  def sing(module) do
+    :inets.start()
+
+    options = [
+      server_name: ~c"frank",
+      server_root: ~c"/tmp",
+      document_root: ~c"/tmp",
+      port: 3000,
+      modules: [module]
+    ]
+
+    {:ok, _pid} = :inets.start(:httpd, options)
+    IO.puts("running on port 3000")
+  end
+
+  defmodule Path do
+    defmacro get(path, do: code) do
+      parts = build_pattern(path)
+
+      quote do
+        def handle(unquote(parts), data) do
+          unquote(code)
+        end
+      end
+    end
+
+    defmacro __before_compile__(_env) do
+      quote do
+        def handle(_, data) do
+          response(404, ~c"not defined")
+        end
+      end
+    end
+
+    defp build_pattern(path) do
+      path = String.trim_leading(path, "/")
+
+      for part <- String.split(path, "/") do
+        cond do
+          String.starts_with?(part, ":") ->
+            # expands (when unquoted) to a variable name
+            {String.to_atom(String.trim_leading(part, ":")), [], nil}
+
+          true ->
+            # literal string match
+            part
+        end
+      end
+    end
+
+    def redirect(path, code \\ 302) do
+      body = [~c"redirecting you to <a href=\"", path, ~c"\">", path, ~c"</a>"]
+      response(code, body, location: path)
+    end
+
+    def sanitize(content) do
+      content = Regex.replace(~r/&/, content, "\\&amp;")
+      content = Regex.replace(~r/</, content, "\\&lt;")
+      content = Regex.replace(~r/>/, content, "\\&gt;")
+      content
+    end
+
+    def response(code, body, headers \\ []) do
+      body =
+        if is_binary(body) do
+          :erlang.bitstring_to_list(body)
+        else
+          body
+        end
+
+      headers =
+        [code: code, content_length: Integer.to_charlist(IO.iodata_length(body))] ++ headers
+
+      {:proceed, [response: {:response, headers, body}]}
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Frank.Path
+
+      def unquote(:do)(data) do
+        [_ | path] = Frank.mod(data, :request_uri)
+        path = :erlang.list_to_bitstring(path)
+        parts = String.split(path, "/")
+        handle(parts, data)
+      end
+
+      @before_compile Path
+    end
+  end
+end
+
+defmodule Test do
+  use Frank
+
+  get "/" do
+    response(200, "<a href='/foo'>go to foo</a>")
+  end
+
+  get "/foo" do
+    response(200, "foo!")
+  end
+
+  get "/hello/:name" do
+    response(200, "greetings #{name}")
+  end
+end
+
+Frank.sing(Test)
